a42561e726bdb79735c659df8c2889f93a2a9513
Divyakeerthi <divyavarma2122@gmail.com>
2026-05-07 16:23:17 +0530

    Delete TaskManager directory

--- TaskManager/src/main/java/com/task/taskmanager/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.task.taskmanager.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.task.taskmanager.entity.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Integer> {
-
-}
